f9a4d6bd1084edcb54edbc49f31c6d47e8aaf163
efsatuanatilki <efsatlk_1987@hotmail.com>
2025-12-20 23:21:36 +0300

    subclass eklendi.

--- app/modules/module_1/implementations.py
+++ app/modules/module_1/implementations.py
@@ -157,6 +157,15 @@ class Bus(Transport):
             self.dolu_koltuk = 0
         print(f"{sayi} yolcu indirildi.Güncel dolu koltuk:{self.dolu_koltuk}")
 
+    def get_info(self):
+        return (
+            f"Bus | id={self.id} | hat={self.__hat_no} | "
+            f"kapasite={self.kapasite} | dolu={self.__dolu_koltuk} | "
+            f"saat={self.__sefer_baslangic_saati} - {self.__sefer_bitis_saati}| "
+            f"aralık={self.__sefer_araligi_dk}dk | "
+            f"durum={self.durum}"
+        )
+
     #  Sınıf metodu örneği (classmethod)
     @classmethod
     def standart_otobus(cls, id: int, hat_no: str, mevcut_lokasyon: str = "Kampüs"):      # Hazır değerlerle hızlıca Bus üretmek için.
@@ -186,13 +195,15 @@ class Bike(Transport):
         id: int,
         mevcut_lokasyon: str,
         durum: str,
-        bisiklet_tipi: str = "normal",   # "normal" / "elektrikli" gibi
-        kirada_mi: bool = False
+        bisiklet_tipi: str ,   # "normal" / "elektrikli" gibi
+        kirada_mi: bool ,
+        
     ):
         # Bisiklet tek kişilik kabul edelim
         super().__init__(id, kapasite=1, mevcut_lokasyon=mevcut_lokasyon, durum=durum)
 
         # Bike'a özel alanlar
+        
         self.__bisiklet_tipi = None
         self.__kirada_mi = None
          # Sefer bilgisi
@@ -200,13 +211,13 @@ class Bike(Transport):
         
         self.bisiklet_tipi= bisiklet_tipi
         self.kirada_mi= kirada_mi
-
+        
     @property
     def bisiklet_tipi(self):
         return self.__bisiklet_tipi
     @bisiklet_tipi.setter
     def bisiklet_tipi(self,value:str):
-        if value not in {"noemal","elektrikli"}:
+        if value not in {"normal","elektrikli"}:
             raise ValueError("bisiklet 'tipi normal' veya 'elektrikli' olmalı.")
         self.__bisiklet_tipi=value
     
@@ -237,12 +248,8 @@ class Bike(Transport):
             print(f"[Bike {self.id}] Bakımda olduğu için kullanılamaz.")
             return
 
-        if self.durum == "seferde":
-            print(f"[Bike {self.id}] Zaten kullanımda.")
-            return
-
         if self.kirada_mi:
-            print(f"[Bike {self.id}] Zaten kirada görünüyor.")
+            print(f"[Bike {self.id}] kirada görünüyor.")
             return
 
         self.hedef_lokasyon = hedef_lokasyon
@@ -268,10 +275,12 @@ class Bike(Transport):
 
     # Abstract metot override: boş kapasite hesaplama
     def bos_kapasite_hesapla(self):
-        # Bisiklet tek kişilik: kiradaysa 0, değilse 1
-        return 0 if self.kirada_mi else 1
+        if self.kirada_mi:
+            return 0
+        else:
+            return 1
 
-    # Nesne metodu örneği: bakıma al / çıkar
+    # Nesne metodu örneği: bakıma al / çıkar                                 #sistem tarafından çağırılır kullanıcıya açık değildir.
     def bakima_al(self):
         if self.durum == "seferde":
             print(f"[Bike {self.id}] Kullanımdayken bakıma alınamaz.")
@@ -285,15 +294,156 @@ class Bike(Transport):
         self.durum = "bos"
         print(f"[Bike {self.id}] Bakımdan çıktı, kullanıma hazır.")
 
-    # Classmethod örneği: hızlıca bisiklet üret
-    @classmethod
-    def standart_bisiklet(cls, id: int, mevcut_lokasyon: str = "Kampüs"):
-        return cls(
-            id=id,
-            mevcut_lokasyon=mevcut_lokasyon,
-            durum="bos",
-            bisiklet_tipi="normal",
-            kirada_mi=False
+    def get_info(self):
+        return (
+            f"Bike | id={self.id} |"
+            f"Tip={self.__bisiklet_tipi} | "
+            f"lokasyon={self.mevcut_lokasyon} | "
+            f"kira durumu={self.__kirada_mi} | "
+            f"durum={self.durum}"
         )
 
-   
+    # Classmethod örneği: hızlıca bisiklet üret
+    @classmethod
+    def bisiklet_olustur(cls, id: int, mevcut_lokasyon: str = "Kampüs", tip: str = "normal"):
+        return cls(
+        id=id,
+        mevcut_lokasyon=mevcut_lokasyon,
+        durum="bos",
+        bisiklet_tipi=tip,
+        kirada_mi=False
+    )
+from __future__ import annotations
+from datetime import datetime
+
+from app.modules.module_1.base import BaseClass1
+
+class Shuttle(Transport):
+     def __init__(
+        self,
+        id:int,
+        kapasite: int,
+        mevcut_lokasyon:str,
+        durum:str,
+        baslangic_duragi:str,
+        bitis_duragi:str,
+        dolu_koltuk:int
+        ):
+        
+        sıper().__init__(id,kapasite,mevcut_lokasyon,durum)
+        self.__baslangic_duragi = None
+        self.__bitis_duragi = None
+        self.__dolu_koltuk = None
+        self.__hedef_lokasyon = None
+
+        self.__sefer_baslangic_saati = 7
+        self.__sefer_bitis_saatii = 22
+        self.__sefer_araligi_dk = 15
+
+        self.__baslangic_duragi = baslangic_duragi
+        self.__bitis_duragi = bitis_duragi
+        self.__dolu_koltuk=dolu_koltuk
+
+@property
+def baslangic_duragi(self):
+    return self.__baslangic_duragi
+@baslangic_duragi.setter
+def baslangic_duragi(self,durak):
+    if not isinstance(durak,str):
+        raise ValueError("Baslangıç durağı metin olmalıdır")
+    durak=durak.strip()
+    if durak =="":
+        raise ValueError("Başlangıç durağı boş olamaz")
+    self.__baslangic_duragi = durak
+
+@property
+def bitis_duragi(self):
+    return self.__bitis_duragi
+@bitis_duragi.setter
+def bitis_duragi(self,durak):
+    if not isinstance(durak,str):
+        raise ValueError("Bitiş durağı metin olmalıdır")
+    durak=durak.strip()
+    if durak=="":
+        raise ValueError("Bitiş durağı boş olamaz.")
+    
+@property
+def dolu_koltuk(self):
+    return self.__dolu_koltuk 
+@dolu_koltuk.setter
+def dolu_koltuk(self,sayi):
+    if not isinstance(sayi,int):
+        raise ValueError("dolu koltuk int olmalıdır.")
+    if sayi<0:
+        raise ValueError("dolu koltuk negatif olamaz.")
+    if sayi>self.kapasite:
+        raise ValueError("Kapasite aşılamaz.")
+    self.__dolu_koltuk= sayi
+
+@staticmethod
+def saat_uygun_mu(baslangic_saat: int, bitis_saat: int) -> bool:
+        simdiki_saat = datetime.now().hour
+        return baslangic_saat <= simdiki_saat < bitis_saat
+
+def binis_yap(self,kisi_sayisi):
+    if not isinstance(kisi_sayisi,int) or kisi_sayisi<=0:
+        raise ValueError("kişi sayısı pozitif int olmalıdır.")
+    if self.__dolu_koltuk + kisi_sayisi > self.kapasite:
+        raise ValueError("Kapasite Aşılamaz.")
+    self.__dolu_koltuk += kisi_sayisi
+
+def inis_yap(self,kisi_sayisi):
+    if not isinstance(kisi_sayisi,int) or kisi_sayisi<=0:
+        raise ValueError("kişi sayısı pozitif int olmalıdır.")
+    if self.__dolu_koltuk - kisi_sayisi <0:
+        raise ValueError("İnen Kişi sayısı dolu koltuktan fazla olamaz.")
+    self.__dolu_koltuk -= kisi_sayisi
+
+def sefer_baslat(self,hedef_lokasyon:str):
+    if not Shuttle.saat_uygun_mu(self.__sefer_baslangic_saati , self.__sefer_bitis_saati):
+        print( f"[Shuttle {self.id}] Sefer saatleri dışında! (07:00 - 22:00)")
+        return
+    if self.durum == "bakimda":
+        print(f"[Shuttle {self.id}] Bakımda olduğu için sefer başlatılamaz.")
+        return
+
+    if self.durum == "seferde":
+        print(f"[Shuttle {self.id}] Zaten seferde.")
+        return
+
+    if not self.hedef_gecerli_mi(hedef_lokasyon):
+        print(
+            f"[Shuttle {self.id}] Hedef geçersiz! "
+            f"Sadece '{self.__baslangic_duragi}' veya '{self.__bitis_duragi}' olabilir."
+        )
+        return
+
+    self.__hedef_lokasyon = hedef_lokasyon
+    self.durum = "seferde"
+
+    print(
+            f"[Shuttle {self.id}] Sefer başladı | "
+            f"{self.__baslangic_duragi} <-> {self.__bitis_duragi} | "
+            f"Hedef: {hedef_lokasyon} | "
+            f"Aralık: {self.__sefer_araligi_dk} dk"
+        )
+    
+def sefer_bitir(self):
+        self.durum = "beklemede"
+        self.__hedef_lokasyon = None
+        print(f"[Shuttle {self.id}] Sefer bitti, beklemede.")
+
+@classmethod
+def shuttle_olustur(cls, id: int):
+        return cls(
+            id=id,
+            kapasite=20,
+            mevcut_lokasyon="Yurtlar",
+            durum="beklemede",
+            baslangic_duragi="Yurtlar",
+            bitis_duragi="Kampüs",
+            dolu_koltuk=0
+        )
+
+
+
